acbf111c27ce78d5ffcfce6789cef744b1f49bc5
Hotblack <ajackrel@gmail.com>
2023-11-30 20:48:58 -0800

    first commit

---
+++ app/src/main/java/com/example/recyclerexample/MainActivity.java
@@ -0,0 +1,34 @@
+/*
+NAME:
+DATE:
+ASSIGNMENT:
+ */
+
+/*
+This assignment will introduce you to RecyclerView, CardView, and  creating a custom layout.
+You will follow the example we do in class.
+Then, you will create your own custom layout for a CardView and successfully implement it into a
+RecyclerView
+REQUIREMENTS:
+-must use the flag images
+-your custom card must be stylish, well formatted, and balanced with any other visual elements
+-must use at least two custom color (colorhunt.co)
+-must use one ImageView and at least two TextView's
+
+Due: Monday (please have it ready as we'll be using your CardView for something else)
+ */
+
+package com.example.recyclerexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
